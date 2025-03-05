b3e0465d03591c0c6bbce8e3bfea3aba03cdcf6e
RWallan <3am.richardwallan@gmail.com>
2025-03-05 00:59:24 -0300

    feat: persist data in storage overwriting or appending

--- insurance_sell/extract.py
+++ insurance_sell/extract.py
@@ -1,11 +1,22 @@
+import tempfile
 from pathlib import Path
+from typing import Any, Optional
 
 import pandas as pd
 from pandera.errors import SchemaError
-from prefect import task
+from prefect import flow, task
+from prefect.futures import wait
 from prefect.logging import get_run_logger
+from prefect.task_runners import ThreadPoolTaskRunner
 from prefect.tasks import exponential_backoff
+
 from insurance_sell.settings import Settings
+from insurance_sell.storage import (
+    BucketClient,
+    check_if_object_exists,
+    get_file_from_storage,
+    send_file_to_storage,
+)
 
 from .schemas import RawInsuranceSell
 
@@ -49,5 +60,112 @@ class Extractor:
 
         except SchemaError as exc:
             _logger.error(f'Failed to validate data schema. {exc}')
-    )
 
+    @task(name='persist-in-storage', tags=TAGS)
+    def _persist_in_storage(
+        self,
+        client: BucketClient,
+        bucket_name: str,
+        input_file: str,
+        output_file: str,
+        *,
+        overwrite: bool,
+    ):
+        """Persist a file to bucket storage verifying if overwrite or not."""
+        _logger = get_run_logger()
+
+        if not overwrite:
+            df = get_file_from_storage(
+                client, bucket_name, input_file, output_file
+            )
+            if isinstance(df, pd.DataFrame):
+                df = pd.concat([df, self._raw_data])
+                df.to_csv(input_file, index=False)
+            else:
+                _logger.error(
+                    (
+                        f'Error to load {output_file} '
+                        f'from {bucket_name}. Overwriting...'
+                    )
+                )
+                self._raw_data.to_csv(input_file, index=False)
+        else:
+            self._raw_data.to_csv(input_file, index=False)
+
+        send_file_to_storage(client, bucket_name, input_file, output_file)
+
+    @flow(
+        name='extract-data-and-persists',
+        task_runner=ThreadPoolTaskRunner(max_workers=2),
+    )
+    def extract(  # noqa: PLR0913
+        self,
+        output_file: str | Path,
+        sources: Optional[list[str]] = None,
+        client: Optional[Any] = None,
+        bucket_name: Optional[str] = None,
+        *,
+        overwrite: bool = False,
+        persist_in_storage: bool = True,
+    ):
+        """Execute all extract data flow.
+
+        This pipeline will:
+            - Extract data from source
+            - Validate with InsuranceSell schema
+            - Persists de data in storage or local
+
+        Args:
+            sources: A list of csv sources.
+            client: A bucket client to use.
+            bucket_name: Bucket name.
+            output_file: Output file name. If `persist_in_storage` is False, need be a local path.
+            overwrite: If True, will overwrite existing data.
+            persist_in_storage: If data will persist in storage.
+        """  # noqa: E501
+        _logger = get_run_logger()
+
+        futures = []
+        sources = sources or self._settings.DATA_SOURCES
+        for source in sources:
+            _logger.info(f'Extracting data for {source.split("/")[-1]}')
+            futures.append(self.fetch_data.submit(source))
+
+        wait(futures)  # wait for the sequence of futures to complete
+
+        has_object = False
+        if persist_in_storage:
+            if bucket_name and client:
+                # Check if can append in existing file
+                if not overwrite:
+                    has_object = check_if_object_exists(
+                        client, 'raw', 'raw.csv'
+                    )
+                if not has_object:
+                    _logger.warning(
+                        (
+                            f'{output_file} not found in {bucket_name}. '
+                            'Overwriting'
+                        )
+                    )
+                    overwrite = True
+
+                # Create a temporary file to save in storage
+                with tempfile.NamedTemporaryFile(suffix='.csv') as f:
+                    self._persist_in_storage(
+                        client,
+                        bucket_name,
+                        f.name,
+                        output_file,
+                        overwrite=overwrite,
+                    ).submit()
+            else:
+                _logger.warning(
+                    (
+                        'Tried to persists in storage but no bucket or '
+                        'client provided. Trying to save locally'
+                    )
+                )
+                self._raw_data.to_csv(output_file, index=False)
+        else:
+            self._raw_data.to_csv(output_file, index=False)
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -52,3 +52,161 @@ def test_fetch_wrong_schema_must_raises(tmp_path, caplog):
             extractor.fetch_data.fn(extractor, str(tmp_path / 'test.csv'))
 
     assert 'Failed to validate data schema' in caplog.text
+
+
+def test_persist_in_storage_must_append(client, tmp_path, monkeypatch):
+    expected_appended_df_length = 4
+    df = pd.DataFrame(
+        {
+            'id': ['1', '2'],
+            'Gender': ['m', 'f'],
+            'Age': [20.0, 25.0],
+            'HasDrivingLicense': [1.0, 0.0],
+            'RegionID': [1.0, 2.0],
+            'Switch': [1.0, 0.0],
+            'VehicleAge': ['<1 year', '>=1 year'],
+            'PastAccident': ['yes', 'no'],
+            'AnnualPremium': ['£123', '£456'],
+            'SalesChannelID': ['1', '2'],
+            'DaysSinceCreated': [1, 2],
+            'Result': [0, 1],
+        }
+    )
+
+    df.to_csv(tmp_path / 'test.csv', index=False)
+
+    def mock_get_file_from_storage(*args):
+        return df
+
+    def mock_put_file_in_storage(*args):
+        pass
+
+    monkeypatch.setattr(
+        'insurance_sell.extract.send_file_to_storage',
+        mock_put_file_in_storage,
+    )
+
+    monkeypatch.setattr(
+        'insurance_sell.extract.get_file_from_storage',
+        mock_get_file_from_storage,
+    )
+
+    with disable_run_logger():
+        extractor._persist_in_storage.fn(
+            extractor,
+            client,
+            'test',
+            str(tmp_path / 'test.csv'),
+            'output.csv',
+            overwrite=False,
+        )
+
+    result_df = pd.read_csv(tmp_path / 'test.csv')
+
+    assert len(result_df) == expected_appended_df_length
+
+
+def test_persist_in_storage_must_overwrite(client, tmp_path, monkeypatch):
+    df = pd.DataFrame(
+        {
+            'id': [1, 2],
+            'Gender': ['m', 'f'],
+            'Age': [20.0, 25.0],
+            'HasDrivingLicense': [1.0, 0.0],
+            'RegionID': [1.0, 2.0],
+            'Switch': [1.0, 0.0],
+            'VehicleAge': ['<1 year', '>=1 year'],
+            'PastAccident': ['yes', 'no'],
+            'AnnualPremium': ['£123', '£456'],
+            'SalesChannelID': [1, 2],
+            'DaysSinceCreated': [1, 2],
+            'Result': [0, 1],
+        }
+    )
+
+    df.to_csv(tmp_path / 'test.csv', index=False)
+
+    def mock_get_file_from_storage(*args):
+        return df
+
+    def mock_put_file_in_storage(*args):
+        pass
+
+    monkeypatch.setattr(
+        'insurance_sell.extract.send_file_to_storage',
+        mock_put_file_in_storage,
+    )
+
+    monkeypatch.setattr(
+        'insurance_sell.extract.get_file_from_storage',
+        mock_get_file_from_storage,
+    )
+
+    with disable_run_logger():
+        extractor._persist_in_storage.fn(
+            extractor,
+            client,
+            'test',
+            str(tmp_path / 'test.csv'),
+            'output.csv',
+            overwrite=True,
+        )
+
+    result_df = pd.read_csv(tmp_path / 'test.csv')
+
+    testing.assert_frame_equal(result_df, df)
+
+
+def test_persist_in_storage_must_overwrite_if_dont_have_past_object(
+    client, tmp_path, monkeypatch, caplog
+):
+    df = pd.DataFrame(
+        {
+            'id': [1, 2],
+            'Gender': ['m', 'f'],
+            'Age': [20.0, 25.0],
+            'HasDrivingLicense': [1.0, 0.0],
+            'RegionID': [1.0, 2.0],
+            'Switch': [1.0, 0.0],
+            'VehicleAge': ['<1 year', '>=1 year'],
+            'PastAccident': ['yes', 'no'],
+            'AnnualPremium': ['£123', '£456'],
+            'SalesChannelID': [1, 2],
+            'DaysSinceCreated': [1, 2],
+            'Result': [0, 1],
+        }
+    )
+
+    df.to_csv(tmp_path / 'test.csv', index=False)
+
+    def mock_get_file_from_storage(*args):
+        return None
+
+    def mock_put_file_in_storage(*args):
+        pass
+
+    monkeypatch.setattr(
+        'insurance_sell.extract.send_file_to_storage',
+        mock_put_file_in_storage,
+    )
+
+    monkeypatch.setattr(
+        'insurance_sell.extract.get_file_from_storage',
+        mock_get_file_from_storage,
+    )
+
+    with caplog.at_level(logging.ERROR):
+        with disable_run_logger():
+            extractor._persist_in_storage.fn(
+                extractor,
+                client,
+                'test',
+                str(tmp_path / 'test.csv'),
+                'output.csv',
+                overwrite=False,
+            )
+
+    result_df = pd.read_csv(tmp_path / 'test.csv')
+
+    assert 'Error to load output.csv from test. Overwriting...' in caplog.text
+    testing.assert_frame_equal(result_df, df)
